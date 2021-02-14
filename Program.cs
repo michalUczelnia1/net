4c7543cf2042378209cbfb68f1790408e0273d70
michalUczelnia1 <64002841+michalUczelnia1@users.noreply.github.com>
2021-02-14 23:30:45 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace dom
+{
+
+
+    class Dodaj
+    {
+        int a;
+        int b;
+        Dodaj(int a,int b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+
+        public override string ToString()
+        {
+            return string.Format(a+b);
+        }
+
+    }
+
+
+	class Odejmij
+	{
+		int a;
+		int b;
+		Odejmij(int a, int b)
+		{
+			this.a = a;
+			this.b = b;
+		}
+
+		public override string ToString()
+		{
+			return string.Format(a - b);
+		}
+
+	}
+
+	class pomnoz
+	{
+		int a;
+		int b;
+		pomnoz(int a, int b)
+		{
+			this.a = a;
+			this.b = b;
+		}
+
+		public override string ToString()
+		{
+			return string.Format(a*b);
+		}
+
+	}
+
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Dodaj d1 = new Dodaj(4, 1);
+            Console.WriteLine(d1);
+
+
+
+        }
+    }
+}
